c154566bd356dd81ebd4f586733009411ab44c9b
charroux <benoit.charroux@free.fr>
2017-11-28 15:31:01 +0100

    adding http

--- src/app/car.service.ts
+++ src/app/car.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Car } from './car';
 import { CARS } from './mock-cars';
@@ -6,11 +7,14 @@ import { CARS } from './mock-cars';
 @Injectable()
 export class CarService {
 
+  constructor(private http: HttpClient) {}
+
   getCars(): Promise<Car[]> {
     return Promise.resolve(CARS);
   }
 
   getCar(id: number): Promise<Car> {
+    /** update this method with this.http.get... see https://angular.io/guide/http) **/
     return Promise.resolve(CARS[id]);
   }
 
