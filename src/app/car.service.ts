8463003eba265ee9fcf7e43e465937c73b07c673
mcsu <591812541@qq.com>
2018-01-09 13:19:10 +0100

    Update car.service.ts
    update http

--- src/app/car.service.ts
+++ src/app/car.service.ts
@@ -1,21 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Headers, Http } from '@angular/http';
 import { Car } from './car';
 import { CARS } from './mock-cars';
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class CarService {
 
-  constructor(private http: HttpClient) {}
+
+  private heroesUrl = 'http://127.0.0.1:8080/voitures';
+  constructor(private http: Http) {}
 
   getCars(): Promise<Car[]> {
-    return Promise.resolve(CARS);
-  }
+      return this.http.get(this.heroesUrl)
+             .toPromise()
+             .then(response => response.json() as Car[])
+             .catch(this.handleError);
+}
 
-  getCar(id: number): Promise<Car> {
+  private handleError(error: any): Promise < any > {
+  console.error('An error occurred', error); // for demo purposes only
+  return Promise.reject(error.message || error);
+}
+
+  getCar(id: number): Promise < Car > {
     /** update this method with this.http.get... see https://angular.io/guide/http) **/
-    return Promise.resolve(CARS[id]);
+    // return Promise.resolve(CARS[id]);
+     return this.http.get(this.heroesUrl)
+             .toPromise()
+             .then(response => response.json() as Car[])
+             .catch(this.handleError);
   }
 
   rent(car) {
